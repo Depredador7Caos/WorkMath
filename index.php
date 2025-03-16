15316fbb59e6cbf20a3b3315b55ca3778ffab0ef
mariana7Va <mariana.hernandez@cbtis171.edu.mx>
2025-03-15 20:46:02 -0600

    a

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
-<?php
-
-    echo "Donde esta mi git...";
-    echo "Donde esta mi git...";
-    echo "Donde esta mi git...";
-    echo "Donde esta mi git...";
-    echo "Donde esta mi git...";
-    echo "Donde esta mi git...";
-    echo "Donde esta mi git...";
-    echo "Donde esta mi git...";
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
