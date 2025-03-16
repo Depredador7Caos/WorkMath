feae7ecbdfda5027d30b6be26993bc46b8ac9d7d
Jose_Julian_CA <jose.julian.celio.arellano@gmail.com>
2025-03-15 22:08:36 -0600

    estilos

---
+++ src/public/layout/footer.php
@@ -0,0 +1,53 @@
+        <footer class="footer bg-color-purple"> 
+            <div class="contetn-footer">
+                <div class="footer-head">
+                    <div class="footer-ctr-title center">
+                        <h2 class="text-color-white font-2">Title de aplication</h2>
+                    </div>
+                    <div class="footer-ctr-btn col-12 center py-1">
+                        <button class="btn btn-secondary btn-md text-color-white col-10" type="button">Empezar ahora</button>
+                    </div>
+                </div>
+                <div class="footer-content py-1 px-1">
+                    <div class="content-footer px-2">
+                        <h2 class="text-color-white font-1-md"> Acerca de nosotros </h2>
+                        <div class="contentenido px-2">
+                            <a href="#">Mision</a>
+                            <a href="#">Manual de "Name"</a>
+                            <a href="#">Contactanos</a>
+                        </div>
+                    </div>
+                    <div class="content-footer px-2">
+                        <h2 class="text-color-white font-1-md"> Ayuda y soporte </h2>
+                        <div class="contentenido px-2">
+                            <a href="#">App</a>
+                            <a href="#">Estado</a>
+                        </div>
+                    </div>
+                    <div class="content-footer px-2">
+                        <h2 class="text-color-white font-1-md"> Cursos </h2>
+                        <div class="contentenido px-2">
+                            <a href="#">Pensamiento Matematico I</a>
+                            <a href="#">Pensamiento Matematico II</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer-footer py-1">
+                    <div class="title-items mt-1">
+                        <h2 class="text-color-white font-2 center"> Nuestras redes sociales</h2>
+                    </div>
+                    <div class="footer-content-items">
+                        <div class="item-content"><i class='bx bxl-discord-alt'></i></div>
+                        <div class="item-content"><i class='bx bxl-instagram-alt'></i></div>
+                        <div class="item-content"><i class='bx bxl-facebook-circle'></i></div>
+                        <div class="item-content"><i class='bx bxl-github'></i></div>
+                        <div class="item-content"><i class='bx bxl-youtube'></i></div>
+                        <div class="item-content"><i class='bx bxl-whatsapp'></i></div>
+                        <div class="item-content"><i class='bx bxl-messenger'></i></div>
+                        <div class="item-content"><i class='bx bxl-twitter'></i></div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
---
+++ src/views/user-register/user_register.php
@@ -0,0 +1,5 @@
+<?php include("../../public/layout/head.php"); ?>
+
+<h1>register users</h1>
+
+<?php include("../../public/layout/footer.php"); ?>
